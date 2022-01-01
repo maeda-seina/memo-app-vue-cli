2df7e7d0813e7331039dacf49900ec312bdc976b
maeda-seina <ayrton19601027@gmail.com>
2022-01-01 10:41:38 +0900

    memoアプリ作成

---
+++ src/components/Memo.vue
@@ -0,0 +1,87 @@
+<template>
+  <section class="alert alert-primary">
+    <ul class="list-group">
+      <li v-for="(memo, index, key) in memos" :key="key" @click="displayEditForm(index)" class="list-group-item list-group-item-action text-left btn">
+        <span>{{ memo.split('\n')[0] }}</span>
+      </li>
+    </ul>
+    <template v-if="!addFlag && !editFlag">
+      <span class="btn btn-info m-2" @click="displayAddForm">＋</span>
+    </template>
+    <template v-else-if="addFlag">
+      <textarea class="mt-5" rows="5" cols="50" v-model="newMemo"></textarea>
+      <div>
+        <button class="btn btn-info m-2" @click="addMemo">Add</button>
+        <transition name="del">
+          <button class="btn btn-info m-2" @click="cancel">Cancel</button>
+        </transition>
+      </div>
+    </template>
+    <template v-if="editFlag">
+      <textarea class="mt-5" rows="5" cols="50" v-model="newMemo"></textarea>
+      <div>
+        <button class="btn btn-info m-2" @click="editMemo">Update</button>
+        <button class="btn btn-info m-2" @click="deleteMemo">Delete</button>
+        <transition name="del">
+          <button class="btn btn-info m-2" @click="editCancel">Cancel</button>
+        </transition>
+      </div>
+    </template>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newMemo: '',
+      memos: [],
+      addFlag: false,
+      editFlag: false,
+      editIndex: null
+    }
+  },
+  methods: {
+    displayAddForm () {
+      this.addFlag = true
+    },
+    cancel () {
+      this.addFlag = false
+    },
+    addMemo () {
+      this.memos.push(this.newMemo)
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+      this.newMemo = ''
+    },
+    displayEditForm (index) {
+      this.editIndex = index
+      this.addFlag = false
+      this.editFlag = true
+      this.newMemo = this.memos[this.editIndex]
+    },
+    editMemo () {
+      this.memos.splice(this.editIndex, 1, this.newMemo)
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+      this.newMemo = ''
+      this.addFlag = false
+      this.editFlag = false
+    },
+    editCancel () {
+      this.addFlag = false
+      this.editFlag = false
+    },
+    deleteMemo () {
+      if (confirm('Are you sure?')) {
+        this.memos.splice(this.editIndex, 1)
+        localStorage.setItem('memos', JSON.stringify(this.memos))
+        this.addFlag = false
+        this.editFlag = false
+        this.newMemo = ''
+      }
+    }
+  },
+  mounted () {
+    this.memos = JSON.parse(localStorage.getItem('memos')) || [];
+  },
+}
+</script>
